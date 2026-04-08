b60f2558294fb7782c68427d2a6e6efb0a9eabb3
Amruth56 <amruth.ts@optym.com>
2026-04-08 12:58:04 +0530

    updated current time state

--- app/src/reusable/components/Navbar.tsx
+++ app/src/reusable/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const navItems = [
   "Home",
@@ -16,25 +16,16 @@ const navItems = [
 ];
 
 const Navbar = () => {
-  const [currentTime, setCurrentTime] = useState("");
-  const time = new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(
+    new Date().toLocaleTimeString(),
+  );
 
   useEffect(() => {
-    const updateTime = () => {
-      const now = new Date();
-      setCurrentTime(
-        now.toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-        })
-      );
-    };
+    const timer = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+    }, 1000);
 
-    updateTime();
-    const interval = setInterval(updateTime, 1000);
-
-    return () => clearInterval(interval);
+    return () => clearInterval(timer);
   }, []);
 
   return (
@@ -44,7 +35,7 @@ const Navbar = () => {
           <div className="flex justify-between items-center w-full">
             <div className="flex gap-4">
               <h1 className="font-bold text-blue-500 text-3xl">O</h1>
-              <nav className="gap-4 flex items-center ml-2">
+              <nav className="gap-4 flex items-center ml-2 cursor-pointer">
                 {navItems.map((item, index) => (
                   <a key={index}>{item.toUpperCase()}</a>
                 ))}
@@ -52,10 +43,14 @@ const Navbar = () => {
             </div>
 
             <div className="flex items-center gap-4 ml-auto">
-              <p className="border p-2 rounded-md text-blue-300">MESSAGE</p>
-              <p className="border p-2 rounded-md text-blue-300">SCAN PRO</p>
-              <p className="border p-2 rounded-md">FTW</p>
-              <p>{time}</p>
+              <p className="border p-2 rounded-md text-blue-300 cursor-pointer">
+                MESSAGE
+              </p>
+              <p className="border p-2 rounded-md text-blue-300 cursor-pointer">
+                SCAN PRO
+              </p>
+              <p className="border p-2 rounded-md cursor-pointer">FTW</p>
+              <p>{currentTime}</p>
               <p> | </p>
               <p>OPTYM</p>
             </div>
